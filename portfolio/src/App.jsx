1fcd6c99c1afb1a6620d947a5a1665e0a1cc605f
albanrss <alban.roussee@epitech.eu>
2025-01-04 00:00:09 +0100

    [Fix] Files and contents

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import Header from "./Header"
+import Body from "./Body"
+import Footer from "./Footer"
 
 function App() {
-
   return (
     <>
+      <Header />
+      <Body />
+      <Footer />
     </>
   )
 }
