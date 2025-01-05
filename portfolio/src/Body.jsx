b0f934cea9a314c82fdfea0765fbb4fed3b4deff
albanrss <alban.roussee@epitech.eu>
2025-01-05 18:21:03 +0100

    [ADD] Profile picture

--- portfolio/src/Body.jsx
+++ portfolio/src/Body.jsx
@@ -5,23 +5,26 @@ function Body() {
       <>
         <div className="main_container">
           <div className="contents">
-            <div id='about-me'>
-              <h2>
-                A propos de moi
-              </h2>
-              <p>
-                Je suis étudiant en <b>informatique</b> à <b>Epitech Nantes</b> passionné par la programmation et les nouvelles technologies. Actuellement en formation, je développe mes compétences dans des domaines variés tels que le développement web, la programmation orientée object, et les algorithmes d'intelligence artificielle.
-              </p>
-              <p>
-                Toujours <b>curieux</b> et <b>motivé</b>, j'aime relever des défis techniques et apprendre de nouvelles compétences pour rester à jour dans ce secteur en constante évolution. Mon objectif est de concevoir des solutions innovantes et performantes pour répondre aux besoins actuels et futurs des entreprises.
-              </p>
+            <div id="about-me">
+              <img id="profile_picture" src="/src/assets/pp.jpg" />
+              <div>
+                <h2>
+                  A propos de moi
+                </h2>
+                <p>
+                  Je suis étudiant en <b>informatique</b> à <b>Epitech Nantes</b> passionné par la programmation et les nouvelles technologies. Je développe mes compétences dans des domaines variés tels que le développement web, la programmation orientée objet, et l'algorithmie.
+                </p>
+                <p>
+                  Toujours <b>curieux</b> et <b>motivé</b>, j'aime relever des défis techniques et apprendre de nouvelles compétences pour rester à jour dans ce secteur en constante évolution. Mon objectif est de concevoir des solutions innovantes et performantes pour répondre aux besoins des entreprises.
+                </p>
+              </div>
             </div>
-            <div id='skills'>
+            <div id="skills">
               <h2>
                 Mes compétences
               </h2>
             </div>
-            <div id='projects'>
+            <div id="projects">
               <h2>
                 Mes projets
               </h2>
